20467133b29716be287b5b5a4fc6c89353b1d6e2
ARSolutions-01 <arhightechsolutions@gmail.com>
2025-01-24 07:03:02 +0000

    updated

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -22,7 +22,7 @@ const Main = () => {
       {/* Sticky WhatsApp Icon */}
       <div className="fixed bottom-6 right-6 z-50">
         <a
-          href="https://wa.me/9958626705?text=Hello,%20I%20need%20assistance"
+          href="https://wa.me/7303556175?text=Hello,%20I%20need%20assistance"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
